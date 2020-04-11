6f0f85c2cb4dc9faa9fc79b0888e762d46dead52
Alex Rocha <rocha8524@gmail.com>
2020-04-11 17:49:01 -0400

    Working on line chart

--- src/components/Chart/chart.jsx
+++ src/components/Chart/chart.jsx
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../API";
+import { Line, Bar } from "react-chartjs-2";
+
+import style from "./chart.module.css";
 
 const Chart = () => {
+    const { dailyData, setDailyData } = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        console.log(dailyData);
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        dailyData[0] 
+        ? (
+            <Line
+                data={{
+                    labels: dailyData(({ date }) => date),
+                    datasets: [{
+                        data: dailyData(({ confirmed }) => confirmed),
+                        label: "Infected",
+                        borderColor: "#3333ff",
+                        fill: true,
+                    }, {
+                        data: dailyData(({ deaths }) => deaths),
+                        label: "Deaths",
+                        borderColor: "#3333ff",
+                        fill: true,
+                    }],
+                }}
+            />) :null
+    );
+
     return (
         <h1>Chart</h1>
     )
