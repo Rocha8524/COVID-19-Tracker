74d088cc1e8ae5b2ffb91c9bb70fa56c247def1c
Alex Rocha <rocha8524@gmail.com>
2020-04-19 16:41:58 -0400

    Updates

---
+++ precache-manifest.4488d71b0ef75332c2764b2494f5dbf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310cb49b3540f9738d6d12714b528bca",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "c4192a176e34f5d75948",
+    "url": "/COVID-19-Tracker/static/css/main.ad6b8e57.chunk.css"
+  },
+  {
+    "revision": "a14d9c113e9d56004124",
+    "url": "/COVID-19-Tracker/static/js/2.4d453fbd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.4d453fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4192a176e34f5d75948",
+    "url": "/COVID-19-Tracker/static/js/main.8880ab50.chunk.js"
+  },
+  {
+    "revision": "63604fc8f43201bcf2e7",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.1de9f299.js"
+  }
+]);
